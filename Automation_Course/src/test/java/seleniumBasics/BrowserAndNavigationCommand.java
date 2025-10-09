d9b111e3133c2a3f52bd84f9e7238864b733d1d1
sadiy <sadiy@DESKTOP-M2KK1QU>
2025-10-09 10:59:58 +0530

    project us coomiting

--- Automation_Course/src/test/java/seleniumBasics/BrowserAndNavigationCommand.java
+++ Automation_Course/src/test/java/seleniumBasics/BrowserAndNavigationCommand.java
@@ -5,16 +5,30 @@ public class BrowserAndNavigationCommand extends Base{
 	{
 		String title = driver.getTitle();  // getTitle method is used to get title 
 		System.out.println(title);
-		String source = driver.getPageSource();
+		String source = driver.getPageSource();  //Get full HTML source of the current page
 		System.out.println(source);
+		String url = driver.getCurrentUrl();  // Get the current page URL
+		System.out.println(url);
+		String handle_id = driver.getWindowHandle();
+		System.out.println(handle_id);     //handle id get
+		
 
+	}
+	public void navigationCommand()
+	{
+		driver.navigate().to("https://www.amazon.in/");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BrowserAndNavigationCommand command = new BrowserAndNavigationCommand();
 		command.intializeBrowser();
-		command.browserCommand();
+		//command.browserCommand();
+		command.navigationCommand();
+		
 		
 
 	}
